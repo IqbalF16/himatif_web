994f8a59f4e4c49d3d9a561bc8418478db8b0b88
Dzyfhuba <hafidz21ub@gmail.com>
2021-01-31 12:24:34 +0700

    mengubah si pendaftar agar langsung memiliki role 'user'

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,10 +64,17 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        /*
+         * When someone registers, automatically gets the role as 'user'
+         */
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        $user->assignRole('user');
+
+        return $user;
     }
 }
