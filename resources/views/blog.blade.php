771aaadd5f3df2cf1965f956dd49346da5e09695
Dzyfhuba <hafidz21ub@gmail.com>
2021-02-05 11:27:43 +0700

    menambahkan card list (column)

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -2,31 +2,37 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-
-                    <!-- ini adalah penggunaan 'blade directive' untuk role -->
-                    @role('admin')
-                    Anda masuk dengan role 'admin'
-                    @endrole
-                    @role('user')
-                    Anda masuk dengan role 'user'
-                    @endrole
-                    @guest
-                    Anda belum login
-                    @endguest
-                </div>
+    <div class="card-columns">
+        <div class="card">
+            <img class="card-img-top" src="{{asset('storage/images/mountain.jpg')}}" alt="Card image" style="width:100%">
+            <div class="card-body">
+                <h4 class="card-title">John Doe</h4>
+                <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                <a href="#" class="btn btn-primary stretched-link">See Profile</a>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="{{asset('storage/images/mountain.jpg')}}" alt="Card image" style="width:100%">
+            <div class="card-body">
+                <h4 class="card-title">John Doe</h4>
+                <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                <a href="#" class="btn btn-primary stretched-link">See Profile</a>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="{{asset('storage/images/mountain.jpg')}}" alt="Card image" style="width:100%">
+            <div class="card-body">
+                <h4 class="card-title">John Doe</h4>
+                <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                <a href="#" class="btn btn-primary stretched-link">See Profile</a>
+            </div>
+        </div>
+        <div class="card">
+            <img class="card-img-top" src="{{asset('storage/images/mountain.jpg')}}" alt="Card image" style="width:100%">
+            <div class="card-body">
+                <h4 class="card-title">John Doe</h4>
+                <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+                <a href="#" class="btn btn-primary stretched-link">See Profile</a>
             </div>
         </div>
     </div>
